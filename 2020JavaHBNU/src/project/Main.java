04bcb595eb45644e614dc29f894ed0c51ff61eb8
ror3665 <ror3665@naver.com>
2020-06-11 06:18:13 +0900

    v0.8 패킷 변환 프로그램
    Command 패턴 적용

--- 2020JavaHBNU/src/project/Main.java
+++ 2020JavaHBNU/src/project/Main.java
@@ -1,60 +1,25 @@
 package project;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		HexFileInputStream hexFileInputStream = HexFileInputStream.getInstance();
-		hexFileInputStream.savePacketsfromFile();
+	public static void main(String[] args) {
 		
-		//HEX 바이너리 값 출력
-		//DisplayPackets displayPackets = new DisplayPackets(hexFileInputStream);
-		//displayPackets.displayAllPackets();
+		MenuItem menuItem = new MenuItem();
+		Interpreter interpreter = Interpreter.getInstance();
 		
-		//PacketInterpreter packetInterpreter = new PacketInterpreter();
-		//packetInterpreter.trimSpacePacketList();
-		//packetInterpreter.organizePacket();
-		//packetInterpreter.createTransportProtocolInterpreter();
-		//기본처리
+		DisplayHexBinaryOnCommand displayHexBinaryOnCommand = new DisplayHexBinaryOnCommand(interpreter);
+		DisplayTCPnUDPonCommand displayTCPnUDPonCommand = new DisplayTCPnUDPonCommand(interpreter);
+		DisplayTCPonCommand displayTCPonCommand = new DisplayTCPonCommand(interpreter);
+		DisplayUDPonCommand displayUDPonCommand = new DisplayUDPonCommand(interpreter);
 		
+		menuItem.setCommand(0, displayHexBinaryOnCommand);
+		menuItem.setCommand(1, displayTCPnUDPonCommand);
+		menuItem.setCommand(2, displayTCPonCommand);
+		menuItem.setCommand(3, displayUDPonCommand);
 		
-		//템플릿 메소드 패턴
-		InterpretedTCP tcp = new InterpretedTCP();
-		InterpretedUDP udp = new InterpretedUDP();
-		
-		
-		ArrayList<NodeInterpretedPacket> interpretedTCPList =   tcp.prepareInterpreter();
-		ArrayList<NodeInterpretedPacket> interpretedUDPList = udp.prepareInterpreter();
-		
-		//컴포지트 패턴 사용
-				TransportProtocolComponent tcpComponent = new TransportProtocol("TCP");
-				TransportProtocolComponent udpCinoinent = new TransportProtocol("UDP");
-				TransportProtocolComponent allInterpretedPackets = new TransportProtocol("모든 분석 정보");
-				
-				
-				allInterpretedPackets.add(tcpComponent);
-				allInterpretedPackets.add(udpCinoinent);
-				
-				ArrayList<NodeInterpretedPacket> list = interpretedTCPList;
-				for(int i=0; i<list.size(); i++) {
-					tcpComponent.add(new NodeInterpretedTCP(list.get(i).getID(), list.get(i).getValue()));
-				}
-				
-				ArrayList<NodeInterpretedPacket> list2 = interpretedUDPList;
-				for(int i=0; i<list2.size(); i++) {
-					udpCinoinent.add(new NodeInterpretedUDP(list2.get(i).getID(), list2.get(i).getValue()));
-				}
-				
-				TransportInterpreter interpreter = new TransportInterpreter(tcpComponent);
-				//interpreter.display();
-				
-				TransportInterpreter interpreter2 = new TransportInterpreter(udpCinoinent);
-				interpreter2.display();
-				
-				TransportInterpreter interpreter3 = new TransportInterpreter(allInterpretedPackets);
-				//interpreter3.display();
+		//menuItem.onButtonWasPushed(0);
+		menuItem.onButtonWasPushed(0);
+
 	}
 
 }
